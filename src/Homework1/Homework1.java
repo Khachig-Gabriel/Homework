9b82504e6ff9b268d5cb2b4d2de828eee3008642
Khachig-Gabriel <khachiggabriel@gmail.com>
2021-09-10 02:02:57 +0400

    Homework 1

---
+++ src/Homework1/Homework1.java
@@ -0,0 +1,56 @@
+package Homework1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Khachig Gabriel");
+
+        int firstvariable;
+        firstvariable = 76;
+        int secondvariable;
+        secondvariable = 1190;
+        System.out.println(firstvariable + secondvariable);
+
+        int thirdvariable = 834;
+        int fourthvariable = 791;
+        System.out.println(thirdvariable + fourthvariable);
+
+        int variable1 = 245;
+        int variable2 = 5;
+        float variable3 = 1005;
+        int variable4 = 28;
+        System.out.println(variable1 / variable2);
+        System.out.println(variable3 / variable4);
+
+        int variable5 = -5;
+        int variable6 = 8 * 6;
+        System.out.println(variable5 + variable6);
+        float variable7 = (55 + 9);
+        int variable8 = 9;
+        System.out.println(variable7 / variable8);
+        int variable9 = 20;
+        int variable10 = -3 * 5;
+        double variable11 = 8;
+        System.out.println(variable9 + variable10 / variable11);
+
+        int variable12 = 5;
+        int variable13 = 15 / 3 * 2;
+        int variable14 = 8 % 3;
+        System.out.println(variable12 + variable13 - variable14);
+
+
+        char armsymbol1 = '\u0549';
+        char armsymbol2 = '\u053d';
+        char armsymbol3 = '\u0531';
+        char armsymbol4 = '\u053b';
+        char armsymbol5 = '\u053f';
+
+        System.out.print(armsymbol2);
+        System.out.print(armsymbol3);
+        System.out.print(armsymbol1);
+        System.out.print(armsymbol4);
+        System.out.print(armsymbol5);
+
+    }
+}
---
+++ src/Homework2/Homework2.java
@@ -0,0 +1,6 @@
+package Homework2;
+
+public class Homework2 {
+
+
+}
