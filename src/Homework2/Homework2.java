1c228789c734dd5ba808ccaec0e87902e6daa209
Khachig-Gabriel <khachiggabriel@gmail.com>
2021-09-10 04:19:55 +0400

    Homework2

--- src/Homework2/Homework2.java
+++ src/Homework2/Homework2.java
@@ -2,5 +2,52 @@ package Homework2;
 
 public class Homework2 {
 
+    public static void main(String[] args) {
 
+        int a = 4;
+        int b = 7;
+
+        if (a > b) {
+            System.out.println("First is bigger");
+        } else if (a < b) {
+            System.out.println("First is smaller");
+        } else {
+            System.out.println("They are equal");
+        }
+
+
+
+
+        char char1 = 'A';
+        char char2 = 'B';
+        char char3 = 'C';
+
+        if (char1 > char2) {
+            System.out.println("A comes last, when arranged in alphabetical order");
+        } else if (char2 > char3 ) {
+            System.out.println("B comes last, when arranged in alphabetical order");
+        } else {
+            System.out.println("C comes last, when arranged in alphabetical order");
+        }
+
+
+
+
+        Boolean redIsHitFromBehind = false;
+        Boolean blackIsHitFromBehind = true;
+
+        if (redIsHitFromBehind == true && blackIsHitFromBehind == true || !redIsHitFromBehind &&
+                !blackIsHitFromBehind) {
+            System.out.println("Invalid State");
+        } else if (redIsHitFromBehind == true && blackIsHitFromBehind == false) {
+            System.out.println("The black car is guilty for the crash");
+        } else {
+            System.out.println("The red car is guilty for the crash");
+        }
+
+
+
+
+
+    }
 }
