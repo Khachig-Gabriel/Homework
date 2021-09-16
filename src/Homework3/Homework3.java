d02d720aa935b34689cb1a83af4a30611cce8bd0
Khachig-Gabriel <khachiggabriel@gmail.com>
2021-09-17 02:00:32 +0400

    Homework3

---
+++ src/Homework3/Homework3.java
@@ -0,0 +1,50 @@
+package Homework3;
+
+import java.util.Scanner;
+
+public class Homework3 {
+
+    public static void main(String[] args) {
+
+        int number1, number2;
+        String operator;
+        int result = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your 1st number: ");
+        number1 = sc.nextInt();
+        System.out.println("Enter your 2nd number: ");
+        number2 = sc.nextInt();
+        System.out.println("Enter the operator in the following way: a - add," +
+                " s - subtract, m - multiply, d - divide ");
+        operator = sc.next();
+        sc.close();
+
+        switch (operator) {
+            case "a":
+                result = number1 + number2;
+                break;
+            case "s":
+                result = number1 - number2;
+                break;
+            case "m":
+                result = number1 * number2;
+                break;
+            case "d":
+                result = number1 / number2;
+                break;
+            default:
+                System.out.println("Error: Please enter a valid input: a - add, " +
+                        "s - subtract, m - multiply, d - divide");
+                break;
+        }
+        System.out.println("The result is: " + result);
+
+
+
+    }
+
+
+
+
+}
