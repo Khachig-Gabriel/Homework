3755671b8d25e19256dfbb6552f08cef87d0e41d
Khachig-Gabriel <khachiggabriel@gmail.com>
2021-10-03 04:32:03 +0400

    Insertion sort algorithm

--- src/homework5/InsertionSortAlgorithm.java
+++ src/homework5/InsertionSortAlgorithm.java
@@ -32,6 +32,5 @@ public class InsertionSortAlgorithm {
         System.out.println();
         System.out.println("Sorted Arrays");
         for (int j = 0; j < array.length; j++) {
-            System.out.println("Array [" + j + "] = " + array[j]); }
-    }
+            System.out.println("Array [" + j + "] = " + array[j]); } }
         }
